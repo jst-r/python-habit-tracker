47c03016ef5df6deefb406193ca0110dad8b557e
jst-r <jstr271@gmail.com>
2024-06-26 17:16:34 +0200

    (feat) stub out the rest of the cli

--- src/ui.py
+++ src/ui.py
@@ -9,3 +9,23 @@ def cli():
 @cli.command(help="Add example data to the tracker")
 def init():
     pass
+
+
+@cli.command()
+def add():
+    pass
+
+
+@cli.command()
+def remove():
+    pass
+
+
+@cli.command()
+def list():
+    pass
+
+
+@cli.command()
+def streak():
+    pass
