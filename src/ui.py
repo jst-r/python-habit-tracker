5574d4872bd7c54608259d8f1d021ff2f860ef8a
jst-r <jstr271@gmail.com>
2024-06-26 18:24:23 +0200

    (feat) adding habits

--- src/ui.py
+++ src/ui.py
@@ -1,19 +1,46 @@
 import click
+import peewee
+
+from data_model import insert_example_data
+from period import Period
+from tracking import new_habit
 
 
 @click.group()
 def cli():
-    pass
-
-
-@cli.command(help="Add example data to the tracker")
-def init():
+    """
+    For more detailed documentation try: command --help
+    """
     pass
 
 
 @cli.command()
-def add():
-    pass
+def init():
+    """
+    Add example data to the tracker.
+    """
+    insert_example_data()
+
+
+@cli.command()
+@click.argument("name")
+@click.option(
+    "--period",
+    "-p",
+    type=click.Choice(["daily", "d", "weekly", "w"], case_sensitive=False),
+    required=True,
+    help="How often do you plan to complete this habit",
+)
+def add(name: str, period: Period):
+    """
+    Create a new habit with the provided name and period.
+
+    Example: add "Do the dishes" -p d
+    """
+    try:
+        new_habit(name, period)
+    except peewee.IntegrityError:
+        click.echo("A habit with this name already exists")
 
 
 @cli.command()
