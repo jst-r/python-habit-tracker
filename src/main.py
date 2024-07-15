f8a065b7b1e5f0d1e666679a382be813f2add884
jst-r <jstr271@gmail.com>
2024-07-15 11:19:59 +0200

    (fix) a proper entry point

--- src/main.py
+++ src/main.py
@@ -1,10 +1,15 @@
 from data_model import Completion, Habit, db
 from ui import cli
 
-if __name__ == "__main__":
+
+def main():
     db.connect()
     db.create_tables([Habit, Completion])
 
     cli()
 
     db.close()
+
+
+if __name__ == "__main__":
+    main()
